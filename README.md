f873ba9df618d9adf59f6c2f012311cf5c25913e
Frankie Bagnardi <f.bagnardi@gmail.com>
2016-11-08 20:16:08 -0700

    mentions reduce for multiple objects and adds relevant test

--- README.md
+++ README.md
@@ -13,4 +13,11 @@ exclusive({a: 1, b: 2}, {b: 2, c: 3});
 // => {a: 1, c: 3}
 ```
 
+## many arguments
+
+Currently it only supports two arguments, but if you have an array of objects:
+
+```js
+var res = objects.reduce(exclusive, {});
+```
 
--- index.test.js
+++ index.test.js
@@ -6,3 +6,15 @@ it('works', () => {
   expect(diff(a, b)).toEqual({y: 2, z: 3});
 });
 
+it('reduce', () => {
+  var objs = [
+    {a: 1},
+    {a: 1, b: 2},
+    {c: 1},
+    {d: 2, b: 4},
+  ];
+
+  var res = objs.reduce(diff, {});
+  expect(res).toEqual({c: 1, d: 2});
+});
+
